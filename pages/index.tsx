438679d18ba3d46168de7d0309329ee22ad86b77
mitzanu <mitzanu@hotmail.com>
2023-06-21 15:46:31 +0300

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,22 @@
+import { Text, Page } from '@vercel/examples-ui'
+import { Chat } from '../components/Chat'
+
+function Home() {
+  return (
+    <Page className="flex flex-col gap-12">
+      <section className="flex flex-col gap-6">
+        <Text variant="h1">From Jessus with love</Text>
+      </section>
+
+      <section className="flex flex-col gap-3">
+        <Text variant="h2">Jessus:</Text>
+        <div className="lg:w-2/3">
+          <Chat />
+        </div>
+      </section>
+    </Page>
+  )
+}
+
+
+export default Home
